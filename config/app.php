f9251840aa328d8dbbf36a3d2570d171a07ed2d0
David Jimenez <davidjimenezdagodigital.com>
2019-09-04 19:32:09 -0500

    Ajuste cors

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Package Service Providers...
